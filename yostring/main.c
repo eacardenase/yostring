2a3fa8797e3ec76aaf338db866d834d311a1d957
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-27 14:24:25 -0500

    feat: create a string one char at a time

--- yostring/main.c
+++ yostring/main.c
@@ -6,15 +6,31 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, const char * argv[]) {
-    char x = 0x21;
+    char x = '!';
     
-    while (x <= 0x7e) {
+    while (x <= '~') {
         printf("%x is %c\n", x, x);
         
         x++;
     }
+    
+    char *start = malloc(5);
+    
+    *start = 'L';
+    *(start + 1) = 'o';
+    *(start + 2) = 'v';
+    *(start + 3) = 'e';
+    *(start + 4) = '\0';
+    
+    printf("%s has %zu characters\n", start, strlen(start));
+    printf("The third letter is %c\n", *(start + 2));
+    
+    free(start);
+    start = NULL;
 
     return 0;
 }
